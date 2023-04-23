beca15c03ff7b193d1027c99a54c3db621e7e9be
Marcio Ramires <marcioramiresgt@gmail.com>
2023-04-23 20:54:19 -0300

    finished

--- Program.cs
+++ Program.cs
@@ -21,10 +21,50 @@ static void Menu()
 
 static void Open() 
 {
+    Console.Clear();
+    Console.WriteLine("Qual o caminho do arquivo?");
+    string path = Console.ReadLine();
 
+    using (var file = new StreamReader(path))
+    {
+        string text = file.ReadToEnd();
+        Console.WriteLine(text);
+    }
+    Console.WriteLine("");
+    Console.ReadLine();
+    Menu();
 }
 
 static void Edit()
 {
+    Console.Clear();
+    Console.WriteLine("Digite seu texto abaixo:");
+    Console.WriteLine("------------------");    
+    string text = "";
 
+    do
+    {
+        text += Console.ReadLine();
+        text += Environment.NewLine;
+    }
+
+    while(Console.ReadKey().Key != ConsoleKey.Escape);
+
+    Save(text);
+}
+
+static void Save(string text)
+{
+    Console.Clear();
+    Console.WriteLine("Qual caminho para salvar o arquivo?");
+    var path = Console.ReadLine();
+
+    using (var file = new StreamWriter(path))
+    {
+        file.Write(text);
+    }
+
+    Console.WriteLine($"Arquivo {path} salvo com sucesso!");
+
+    Menu();
 }
