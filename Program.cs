1338cc075b32ee2061699b31f6dca9d03c315288
Marcio Ramires <marcioramiresgt@gmail.com>
2023-04-23 15:26:48 -0300

    first commit - basic menu

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿Menu();
+
+static void Menu()
+{
+    Console.Clear();
+    Console.WriteLine("O que você deseja fazer?");
+    Console.WriteLine("1 - Abrir um arquivo existente");
+    Console.WriteLine("2 - Criar um novo arquivo");
+    Console.WriteLine("0 - Sair");
+    short option = short.Parse(Console.ReadLine());
+
+
+    switch (option)
+    {
+        case 0: System.Environment.Exit(0); break;
+        case 1: Open(); break;
+        case 2: Edit(); break;
+        default: Menu(); break;
+    }
+}
+
+static void Open() 
+{
+
+}
+
+static void Edit()
+{
+
+}
